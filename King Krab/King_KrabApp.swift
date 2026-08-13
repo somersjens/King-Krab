cadcb2e24a607cb51e9e8ae643a13cbd56c49f48
Jens Somers <somersjens@gmail.com>
2026-08-13 20:16:03 +0200

    Initial Commit

---
+++ King Krab/King_KrabApp.swift
@@ -0,0 +1,17 @@
+//
+//  King_KrabApp.swift
+//  King Krab
+//
+//  Created by Jens Somers on 13/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct King_KrabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
